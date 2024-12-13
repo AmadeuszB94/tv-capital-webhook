95c8925ee49599317b2fa5c8b83f233dcf549e65
AmadeuszB94 <am.zg@icloud.com>
2024-12-13 18:21:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, request, jsonify
+import threading
+import time
+import requests
 
 app = Flask(__name__)
 
+# Funkcja pingująca Render, aby utrzymać usługę aktywną
+def ping():
+    url = "https://tv-capital-webhook.onrender.com"  # Twój URL na Render
+    while True:
+        try:
+            response = requests.get(url)
+            print(f"Ping sent to {url}, status code: {response.status_code}")
+        except Exception as e:
+            print(f"Error pinging {url}: {e}")
+        time.sleep(30)  # Ping co 30 sekund
+
+# Uruchomienie funkcji ping w tle
+thread = threading.Thread(target=ping)
+thread.daemon = True  # Wątek zakończy się automatycznie, gdy aplikacja się zamknie
+thread.start()
+
+# Endpoint obsługujący zamówienia z TradingView
 @app.route('/api/v1/orders', methods=['POST'])
 def handle_order():
     data = request.json
